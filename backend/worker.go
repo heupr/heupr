6e0c2fafabb7798632d2d02f4aba350fa0c11ba6
forstmeier <john.forstmeier@gmail.com>
2018-03-30 15:41:30 -0400

    Construct initial worker logic
    Description:
    This is the initial check-in for the actual worker.start() logic;
    this is likely to change/get updated.
    
    I've included a new work struct whose fields hold the values that
    would be returned by the database query methods.
    
    There is also a new dispatcher function (no unit test yet); there is
    a new argument (vs the legacy version) so that we don't need to link
    to the server (this value will change once we implement the
    sync.Map type).
    
    Note: WIP check-in
    Note: There is (intentionally) missing processing logic currently
    Note: Logging still needs to be implemented (for error scenarios)
    Note: An initial unit test has been constructed for worker.start but
          I am not entirely sure it is correct/accurate (needs further
          evaluation)

--- backend/worker.go
+++ backend/worker.go
@@ -4,39 +4,76 @@ import (
 	"heupr/backend/response/preprocess"
 )
 
+var (
+	workQueue   = make(chan *work, 100)
+	workerQueue chan chan *work
+)
+
 type work struct {
 	repoID      int64
-	integration integration
-	setting     settings
+	settings    *settings
+	integration *integration
 	events      []*preprocess.Container
 }
 
 type worker struct {
-	id    int
-	work  chan work
-	queue chan chan work
-	repos map[int64]*repo
-	quit  chan bool
+	id      int
+	work    chan *work
+	workers chan chan *work
+	repos   map[int64]*repo
+	quit    chan bool
 }
 
 func (w *worker) start() {
 	go func() {
 		for {
-			w.queue <- w.work
+			w.workers <- w.work
 			select {
-			case newWork := <-w.work:
-				_ = newWork
-				// evaluate incoming work
-				// if integration present
-				// - initialize new repo w/ client and assets into s.repos
-				// if settings present + if repo key exists
-				// - set pathways + initialize responses
-				// if events present + if repo key exists
-				// - check to see if new integration is present in work struct
-				// - pass events into training/predicting as needed
+			case wk := <-w.work:
+				if wk.integration != nil && wk.settings != nil {
+					r, err := newRepo(wk.settings, wk.integration)
+					_ = err // TODO: Log this result.
+					w.repos[wk.repoID] = r
+				} else if wk.integration == nil && wk.settings != nil {
+					r, ok := w.repos[wk.repoID]
+					_ = ok // TODO: Log this result.
+					err := r.parseSettings(wk.settings, wk.repoID)
+					_ = err // TODO: Log this result.
+				}
+
+				if len(wk.events) != 0 {
+					// call predict on events
+				}
 			case <-w.quit:
 				return
 			}
 		}
 	}()
+}
+
+func dispatcher(repos map[int64]*repo, count int) error {
+	workerQueue = make(chan chan *work, count)
+
+	for i := 0; i < count; i++ {
+		w := &worker{
+			id:      i + 1,
+			work:    make(chan *work),
+			workers: workerQueue,
+			repos:   repos,
+			quit:    make(chan bool),
+		}
+		w.start()
+	}
+
+	go func() {
+		for {
+			wk := <-workQueue
+			go func() {
+				wkrs := <-workerQueue
+				wkrs <- wk
+			}()
+		}
+	}()
+
+	return nil
 }
--- backend/worker_test.go
+++ backend/worker_test.go
@@ -1,9 +1,41 @@
 package backend
 
 import (
+	"reflect"
 	"testing"
 )
 
 func Test_start(t *testing.T) {
+	w := &worker{
+		work:    make(chan *work),
+		workers: make(chan chan *work),
+		repos:   make(map[int64]*repo),
+		quit:    make(chan bool),
+	}
 
+	defer close(w.work)
+	defer close(w.workers)
+	defer close(w.quit)
+
+	tests := []struct {
+		desc string
+		wk   *work
+		expt map[int64]*repo
+		pass bool
+	}{
+		{"blank work and worker arguments", new(work), make(map[int64]*repo), false},
+	}
+
+	for i := range tests {
+		w.start()
+
+		go func() {
+			w.work <- tests[i].wk
+			w.quit <- true
+		}()
+
+		if !reflect.DeepEqual(w.repos, tests[i].expt) {
+			t.Error("no match")
+		}
+	}
 }
