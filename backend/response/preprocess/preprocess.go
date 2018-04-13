4ff4838e06ff5e779c3d27992a044b792e8310b8
forstmeier <john.forstmeier@gmail.com>
2018-04-12 21:07:38 -0400

    Correcting dumb typo
    Description:
    Self-explanatory. :unamused:

--- backend/response/preprocess/preprocess.go
+++ backend/response/preprocess/preprocess.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 )
 
-// Container is the generalied internal object for processing.
+// Container is the generalized internal object for processing.
 type Container struct {
 	repoID  int64
 	event   string
