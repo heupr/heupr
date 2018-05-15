c321a549d4d929be8b75d7335881b34b8ccc04ab
forstmeier <john.forstmeier@gmail.com>
2018-05-15 16:59:40 -0400

    Add initial TOML database reading logic
    Description:
    This is (preliminary) logic for retrieving entries from the TOML
    table; I haven't written any actual queries statements. Couple of
    assumptions I've made:
    - the table contains:
      - the repo ID (as int64)
      - the TOML file contents (as string)
    - the readSettings method is multi-use (but this is not likely to
      remain the case permanently)
    
    This is more of a progress move rather than a final piece of code.
    
    Note: WIP check-in
    Note: No unit tests have been written for database methods

--- backend/database.go
+++ backend/database.go
@@ -2,6 +2,9 @@ package backend
 
 import (
 	"database/sql"
+	"fmt"
+
+	"github.com/BurntSushi/toml"
 
 	"heupr/backend/process/preprocess"
 )
@@ -29,8 +32,29 @@ func (d *database) readIntegrations(query string) (map[int64]*preprocess.Integra
 }
 
 func (d *database) readSettings(query string) (map[int64]*preprocess.Settings, error) {
-	// reads in all settings; called by Start
-	return nil, nil
+	settings := make(map[int64]*preprocess.Settings)
+
+	rows, err := d.sqlDB.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("database read settings: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int64
+		var content string
+		if err := rows.Scan(&id, &content); err != nil {
+			return nil, fmt.Errorf("database read settings row scan: %v", err)
+		}
+
+		set := preprocess.Settings{}
+		if _, err := toml.Decode(content, &set); err != nil {
+			return nil, fmt.Errorf("database read settings toml decode: %v", err)
+		}
+		settings[id] = &set
+	}
+
+	return settings, nil
 }
 
 func (d *database) readEvents(query string) (map[int64][]*preprocess.Container, error) {
